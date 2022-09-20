8c4d5c3e2aa8e59716057ee58f07ba67fd5d792c
Elijah Patric <foreverelijah@gmail.com>
2022-09-20 17:22:30 -0500

    Displaying Cell Info - Section 3 - Do Step 1

---
+++ Today/ListViewController/ReminderListViewController+DataSource.swift
@@ -0,0 +1,21 @@
+//
+//  ReminderListViewController+DataSource.swift
+//  Today
+//
+//  Created by Elijah on 9/20/22.
+//
+import UIKit
+
+extension ReminderListViewController {
+    typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Int, String>
+    
+    func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, id: String) {
+        let reminder = Reminder.sampleData[indexPath.item]
+        var contentConfiguration = cell.defaultContentConfiguration()
+        contentConfiguration.text = reminder.title
+        contentConfiguration.secondaryText = reminder.dueDate.dayAndTimeText
+        contentConfiguration.secondaryTextProperties.font = UIFont.preferredFont(forTextStyle: .caption1)
+        cell.contentConfiguration = contentConfiguration
+    }
+}
