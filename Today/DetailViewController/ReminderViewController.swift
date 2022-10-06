d96edeb3f7e46c33c4d622cfd7efcb2f389a2c0c
Elijah Patric <foreverelijah@gmail.com>
2022-10-06 17:10:57 -0500

    Loading Reminders - Section 1 - Do Step 1

--- Today/DetailViewController/ReminderViewController.swift
+++ Today/DetailViewController/ReminderViewController.swift
@@ -16,6 +16,7 @@ class ReminderViewController: UICollectionViewController {
         }
     }
     var workingReminder: Reminder
+    var isAddingNewReminder = false
     var onChange: (Reminder)->Void
     private var dataSource: DataSource!
 
@@ -50,7 +51,11 @@ class ReminderViewController: UICollectionViewController {
         if editing {
             prepareForEditing()
         } else {
-            prepareForViewing()
+            if !isAddingNewReminder {
+                prepareForViewing()
+            } else {
+                onChange(workingReminder)
+            }
         }
     }
     
---
+++ Today/Models/CAGradientLayer+ListStyle.swift
@@ -0,0 +1,35 @@
+//
+//  CAGradientLayer+ListStyle.swift
+//  Today
+//
+//  Created by Elijah on 10/6/22.
+//
+
+import UIKit
+
+extension CAGradientLayer {
+    static func gradientLayer(for style: ReminderListStyle, in frame: CGRect) -> Self {
+        let layer = Self()
+        layer.colors = colors(for: style)
+        layer.frame = frame
+        return layer
+    }
+    
+    private static func colors(for style: ReminderListStyle) -> [CGColor] {
+        let beginColor: UIColor
+        let endColor: UIColor
+        
+        switch style {
+        case .all:
+            beginColor = UIColor(named: "TodayGradientAllBegin")!
+            endColor = UIColor(named: "TodayGradientAllEnd")!
+        case .future:
+            beginColor = UIColor(named: "TodayGradientFutureBegin")!
+            endColor = UIColor(named: "TodayGradientFutureEnd")!
+        case .today:
+            beginColor = UIColor(named: "TodayGradientTodayBegin")!
+            endColor = UIColor(named: "TodayGradientTodayEnd")!
+        }
+        return [beginColor.cgColor, endColor.cgColor]
+    }
+}
